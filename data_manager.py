77c407739434825ea9c5030a9e54e0e75e982e71
PavlosPo <paulospoulos7@gmail.com>
2022-12-15 15:25:15 +0200

    Updates the DataManagment Class

--- data_manager.py
+++ data_manager.py
@@ -1,11 +1,20 @@
 import os
 import requests
+
 SHEET_HEADERS = {
     "Authorization": os.environ["SHEET_AUTHORIZATION"],
     "Content-Type": "application/json",
 }
 SHEET_ENDPOINT = "https://api.sheety.co/be61e1838b9166a7c340361536e14c49/flightDeals/prices"
+
+
 class DataManager:
-    #This class is responsible for talking to the Google Sheet.
+    # This class is responsible for talking to the Google Sheet.
     def __init__(self):
+        self.data = ''
+        self.get_data()
+
+    def get_data(self):
         self.data = requests.get(url=SHEET_ENDPOINT, headers=SHEET_HEADERS).json()
+        return self.data
+
