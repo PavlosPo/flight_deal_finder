eb64e972e4becb15c68a521666adbaedce2c3ece
PavlosPo <paulospoulos7@gmail.com>
2022-12-16 11:36:07 +0200

    Creating the main file

--- data_manager.py
+++ data_manager.py
@@ -1,20 +1,23 @@
 import os
 import requests
-
+SHEET_ENDPOINT = "https://api.sheety.co/be61e1838b9166a7c340361536e14c49/flightDeals/prices"
 SHEET_HEADERS = {
-    "Authorization": os.environ["SHEET_AUTHORIZATION"],
+    "Authorization": os.getenv(key='SHEET_AUTHORIZATION'),
     "Content-Type": "application/json",
 }
-SHEET_ENDPOINT = "https://api.sheety.co/be61e1838b9166a7c340361536e14c49/flightDeals/prices"
 
 
 class DataManager:
     # This class is responsible for talking to the Google Sheet.
     def __init__(self):
-        self.data = ''
+        # Gets the data
+        self.data = None
         self.get_data()
 
+    # Data fetch
     def get_data(self):
-        self.data = requests.get(url=SHEET_ENDPOINT, headers=SHEET_HEADERS).json()
+        response = requests.get(url=SHEET_ENDPOINT, headers=SHEET_HEADERS)
+        response.raise_for_status()
+        self.data = response.json()
         return self.data
 
--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+import os
+
+from data_manager import DataManager
+
+print(os.getenv("SHEET_AUTORIZATION"))
+sheet_data = DataManager()
+print(sheet_data.data)
