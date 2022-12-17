ddb985aa9baf70735dbc202794a58c599fe8fccf
PavlosPo <paulospoulos7@gmail.com>
2022-12-17 15:02:39 +0200

    Completed the app

--- data_manager.py
+++ data_manager.py
@@ -3,58 +3,32 @@ import os
 import requests
 from flight_search import FlightSearch
 
-SHEET_ENDPOINT = "https://api.sheety.co/be61e1838b9166a7c340361536e14c49/flightDeals/prices"
-SHEET_HEADERS = {
+SHEETY_PRICES_ENDPOINT = "https://api.sheety.co/be61e1838b9166a7c340361536e14c49/flightDeals/prices"
+SHEETY_HEADERS = {
     "Authorization": os.getenv('SHEETAUTHORIZATION'),
     "Content-Type": "application/json",
 }
 
 
 class DataManager:
-    # This class is responsible for talking to the Google Sheet.
     def __init__(self):
-        # Gets the data
-        self.data = None
-        self.get_data()  # Fetch the online dataset
-        self.update_empty_iotcodes()  # Updates the data argument with str 'TESTING'
-        self.upload_data()  # Uploads all data if id is not specified, else only the specified id
-        self.upload_city_codes()  # Retrives and uploads the IATA Codes
+        self.destination_data = {}
 
-    # Data fetch
-    def get_data(self):
-        response = requests.get(url=SHEET_ENDPOINT, headers=SHEET_HEADERS)
-        response.raise_for_status()
-        self.data = response.json()['prices']
+    def get_destination_data(self):
+        response = requests.get(url=SHEETY_PRICES_ENDPOINT)
+        data = response.json()
+        self.destination_data = data["prices"]
+        return self.destination_data
 
-    # Locally updates the data
-    def update_empty_iotcodes(self):
-        for index, current_city in enumerate(self.data):
-            if current_city['iataCode'] == '':
-                # Current city has not a iataCode
-                self.data[index]['iataCode'] = "TESTING"
-
-    # Updates data online
-    def upload_data(self, given_id=None):  # uploads the current data online.
-        # Updates each row of data per loop
-        if given_id is None:
-            for current_city in self.data:
-                current_city_id = current_city['id']
-                data_to_upload = {'price': current_city}  # Formats it the way Sheety wants to.
-                requests.put(url=f"{SHEET_ENDPOINT}/{current_city_id}", json=data_to_upload, headers=SHEET_HEADERS)
-        elif given_id is not None:
-            current_city_id = given_id
-            current_city_data = self.data[given_id - 2]  # The first id is 2, so the first element of list is 2 - 2
-            data_to_upload = {'price': current_city_data}  # Formats it the way Sheety wants to.
-            requests.put(url=f"{SHEET_ENDPOINT}/{current_city_id}", json=data_to_upload, headers=SHEET_HEADERS)
-
-    # Updates and Uploads the city IATA codes from KIWI Api
-    def upload_city_codes(self):
-        # Get the city codes via FlightSearch Class
-        city_code_generator = FlightSearch()  # class for finding the iata code
-        for index, current_city in enumerate(self.data):
-            city_id = current_city['id']  # Id for the city
-            city_name = current_city['city']  # The city name to find the iata code
-            current_city['iataCode'] = city_code_generator.get_destination_code(city_name)  # Get the iata code
-            response = requests.put(url=F"{SHEET_ENDPOINT}/{city_id}", params=current_city, headers=SHEET_HEADERS)
-            response.raise_for_status()
-            self.data[index] = current_city  # Update it also to the class
+    def update_destination_codes(self):
+        for city in self.destination_data:
+            new_data = {
+                "price": {
+                    "iataCode": city["iataCode"]
+                }
+            }
+            response = requests.put(
+                url=f"{SHEETY_PRICES_ENDPOINT}/{city['id']}",
+                json=new_data
+            )
+            print(response.text)
--- flight_search.py
+++ flight_search.py
@@ -1,23 +1,56 @@
-# generates the corresponding IATA Codes for each City.
-import os
-
 import requests
-KIWI_ENDPOINT = "https://api.tequila.kiwi.com/locations/query"
-KIWI_PARAMETERS = {
-    "term": None,  # We need to find it
-    "location_types": "city",
-    "limit": 1
-}
-KIWI_HEADERS = {
-    "apikey": os.getenv('KIWI_API')
-}
+from flight_data import FlightData
+
+TEQUILA_ENDPOINT = "https://tequila-api.kiwi.com"
+TEQUILA_API_KEY = ""
 
 
 class FlightSearch:
-    # This class is responsible for talking to the Flight Search API.
-    def get_destination_code(self, city_name) -> str:
-        KIWI_PARAMETERS['term'] = city_name
-        response = requests.get(url=KIWI_ENDPOINT, params=KIWI_PARAMETERS, headers=KIWI_HEADERS)
-        response.raise_for_status()
-        city_code = response.json()['locations'][0]['code']
-        return city_code
+
+    def get_destination_code(self, city_name):
+        location_endpoint = f"{TEQUILA_ENDPOINT}/locations/query"
+        headers = {"apikey": TEQUILA_API_KEY}
+        query = {"term": city_name, "location_types": "city"}
+        response = requests.get(url=location_endpoint, headers=headers, params=query)
+        results = response.json()["locations"]
+        code = results[0]["code"]
+        return code
+
+    def check_flights(self, origin_city_code, destination_city_code, from_time, to_time):
+        headers = {"apikey": TEQUILA_API_KEY}
+        query = {
+            "fly_from": origin_city_code,
+            "fly_to": destination_city_code,
+            "date_from": from_time.strftime("%d/%m/%Y"),
+            "date_to": to_time.strftime("%d/%m/%Y"),
+            "nights_in_dst_from": 7,
+            "nights_in_dst_to": 28,
+            "flight_type": "round",
+            "one_for_city": 1,
+            "max_stopovers": 0,
+            "curr": "GBP"
+        }
+
+        response = requests.get(
+            url=f"{TEQUILA_ENDPOINT}/v2/search",
+            headers=headers,
+            params=query,
+        )
+
+        try:
+            data = response.json()["data"][0]
+        except IndexError:
+            print(f"No flights found for {destination_city_code}.")
+            return None
+
+        flight_data = FlightData(
+            price=data["price"],
+            origin_city=data["route"][0]["cityFrom"],
+            origin_airport=data["route"][0]["flyFrom"],
+            destination_city=data["route"][0]["cityTo"],
+            destination_airport=data["route"][0]["flyTo"],
+            out_date=data["route"][0]["local_departure"].split("T")[0],
+            return_date=data["route"][1]["local_departure"].split("T")[0]
+        )
+        print(f"{flight_data.destination_city}: £{flight_data.price}")
+        return flight_data
